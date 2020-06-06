939f345a7e401e0384e64af0c7dbdca1957d444a
Sujith <sujithchandrasekaran@gmail.com>
2020-06-06 22:51:52 +0530

    Same title display issue resolved

--- ImageHoster/src/main/java/ImageHoster/repository/ImageRepository.java
+++ ImageHoster/src/main/java/ImageHoster/repository/ImageRepository.java
@@ -53,7 +53,7 @@ public class ImageRepository {
     public Image getImageByTitle(String title) {
         EntityManager em = emf.createEntityManager();
         try {
-            TypedQuery<Image> typedQuery = em.createQuery("SELECT i from Image i where i.title =:title", Image.class).setParameter("title", title);
+            TypedQuery<Image> typedQuery = em.createQuery("SELECT i from Image i where  i.title=:title ", Image.class).setParameter("title", title);
             return typedQuery.getSingleResult();
         } catch (NoResultException nre) {
             return null;
